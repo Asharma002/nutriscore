1a586f402ca6fe74333ea1767604f84f5eddc569
ANSHUMAN SHARMA <anshumansharma179@gmail.com>
2024-02-07 23:00:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import gdown
 
-model_url  = 'https://drive.google.com/uc?id=1xDjhX6HAt_iOUNOLSDQcfsGDhwl02q5q'
+model_url  = 'https://drive.google.com/uc?id=1IMOjyRb4fL8FcdE9kZhlXVDt4n2se07n'
 
 output_path = 'my_model3.hdf5'
 
