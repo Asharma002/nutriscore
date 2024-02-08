f4f64bd9bb4fde59a9962714fe7f9007fbb62aff
ANSHUMAN SHARMA <anshumansharma179@gmail.com>
2024-02-08 16:09:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import emoji
 import keras
 
 # Define the URL of the model file on Google Drive and the output path
-model_url = 'https://drive.google.com/uc?id=1k0riAPbKWXMOxlCLgY9tblIR7et86rMT'
+model_url = 'https://drive.google.com/uc?id=12077yIkpCaAkE8gb-H_Kq7SCXtv6sDPs'
 output_path = 'my_model2.hdf5'
 
 # Download the model from Google Drive
